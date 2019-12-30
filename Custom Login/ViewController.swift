a549c584328953b2c21e949af47b3c2cd7a2c315
Steven Hertz <hertzsteven@icloud.com>
2019-12-30 07:57:47 -0500

    Initial Commit

---
+++ Custom Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom Login
+//
+//  Created by Steven Hertz on 12/30/19.
+//  Copyright © 2019 DevelopItSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
