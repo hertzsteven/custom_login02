8252c1290f9c577963cdebd9c9d53733fe013960
Steven Hertz <hertzsteven@icloud.com>
2020-01-06 20:36:44 -0500

    I think that e-mail verification and voter stuff is working

--- Custom Login/ViewControllers/SignUpViewController.swift
+++ Custom Login/ViewControllers/SignUpViewController.swift
@@ -12,6 +12,9 @@ import Firebase
 
 class SignUpViewController: UIViewController {
     
+    let db = Firestore.firestore()
+
+    
     @IBOutlet weak var firstNameTextField: UITextField!
     
     @IBOutlet weak var lastNameTextField: UITextField!
@@ -47,95 +50,213 @@ class SignUpViewController: UIViewController {
             let lastName = lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
         else { return  }
         
+        /// Create the user in firebase
         Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-            if let err = error {
-                self.showError(error: "Error Creating User \(err.localizedDescription)")
-            } else {
-                /// user created now store it
-                print("Creating the user")
+            guard error == nil else {
+                self.showError(error: "Error Creating User \(error?.localizedDescription ?? "no value")"); return
+             }
+            
+            authResult?.user
+            let docRef = self.db.collection("loginVerification").document("logins")
+            
+            docRef.getDocument { (document, error) in
                 
+                /// handle the all the possible errors getting to the api Key
                 
-                /*
-                /// Delete the user
-                let user = authResult!.user
-
-                user.delete { error in
-                  if let error = error {
-                    // An error happened.
-                    self.showError(error: "Error deleting user \(error.localizedDescription)")
-                    print("error deleting user")
-                  } else {
-                    // Account deleted.
-                    print("Account deleted")
-                  }
+                /// - make sure the dicument exists if not it is a fatal error
+                guard let document = document, document.exists else {
+                    print("Document not found:"); return
                 }
-                */
                 
-                /* Send e-mail confirmation
-                Auth.auth().currentUser?.sendEmailVerification { (error) in
-                    if let error = error {
-                        self.showError(error: "Error send e-mail verification \(error.localizedDescription)")
-                    } else {
-                        print("email sent")
+                /// - get the record dictionary  &  get the email field from the record dicionary which is also a dictionary (or map) in their lingo
+                guard let record = document.data(), let emails = record["emails"] as? [String: String] else {
+                    fatalError("Could not get the email field in the logins document")
+                }
+                
+                /// - look for the value from the dictionary with the key of ""hertzsteven@icloud.com"
+                guard let userEmail = authResult?.user.email, let organization =  emails[userEmail] else {
+                    
+                    print("email address: \(String(describing: authResult?.user.email)) is not found")
+                    self.showError(error: "email address: \(String(describing: authResult?.user.email)) is not found")
+                    ///Delete the user
+                    let user = authResult!.user
+                    
+                    user.delete { error in
+                        if let error = error {
+                            // An error happened.
+                            self.showError(error: "Error deleting user \(error.localizedDescription)")
+                            print("error deleting user")
+                        } else {
+                            // Account deleted.
+                            print("Account deleted")
+                        }
                     }
                     
-                }
-                */
-
-               /*  Create the document
-                let db = Firestore.firestore()
-                
-                let docRef = db.collection("apiKeys").document("ydeSchool")
-                docRef.getDocument { (document, error) in
-                    if let document = document, document.exists {
-                        guard let record = document.data(), let teacherAPIKey = record["teacherAPIKey"] as? String
-                        else {fatalError("could not convert it to a non optional")}
-                        
-                        let uid = authResult!.user.uid
-                        var ref: DocumentReference? = nil
-                        
-                        
-                        // Add a new document in collection "cities"
-                        db.collection("users").document(uid).setData([
-                            "firstName": firstName,
-                            "last": lastName,
-                            "uid": uid,
-                            "apiKey": teacherAPIKey
-                        ]) { err in
-                            if let err = err {
-                                print("Error writing document: \(err)")
-                            } else {
-                                print("Document successfully written!")
-                            }
-                        }
                     
-                        
-//                        ref = db.collection("users").addDocument(data: [
+                     return
+                }
+                
+                /// - Now that the value is gottenyou could create Api Record for the user
+                
+                /// senf verification e-mail
+                Auth.auth().currentUser?.sendEmailVerification { (error) in
+                    guard error == nil else {
+                         self.showError(error: "Error send e-mail verification \(error?.localizedDescription ?? "no value")"); return
+                      }
+
+                    /// build record
+                    guard let x = authResult?.user.uid else {fatalError("could not get the uid")}
+                    self.createAPIRecordForUser(with: organization, uid: x)
+
+                    /// log user off
+                    try! Auth.auth().signOut()
+                    
+                    }
+                }
+                
+            }
+        
+    }
+
+        
+        
+//
+//
+//
+//        /// Create the user in firebase
+//        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+//            if let err = error {
+//                self.showError(error: "Error Creating User \(err.localizedDescription)")
+//            } else {
+//                /// user created now store it
+//                print("Creating the user")
+//
+//
+//                /*
+//                /// Delete the user
+//                let user = authResult!.user
+//
+//                user.delete { error in
+//                  if let error = error {
+//                    // An error happened.
+//                    self.showError(error: "Error deleting user \(error.localizedDescription)")
+//                    print("error deleting user")
+//                  } else {
+//                    // Account deleted.
+//                    print("Account deleted")
+//                  }
+//                }
+//                */
+//
+//                /* Send e-mail confirmation
+//                Auth.auth().currentUser?.sendEmailVerification { (error) in
+//                    if let error = error {
+//                        self.showError(error: "Error send e-mail verification \(error.localizedDescription)")
+//                    } else {
+//                        print("email sent")
+//                    }
+//
+//                }
+//                */
+//
+//               /*  Create the document
+//                let db = Firestore.firestore()
+//
+//                let docRef = db.collection("apiKeys").document("ydeSchool")
+//                docRef.getDocument { (document, error) in
+//                    if let document = document, document.exists {
+//                        guard let record = document.data(), let teacherAPIKey = record["teacherAPIKey"] as? String
+//                        else {fatalError("could not convert it to a non optional")}
+//
+//                        let uid = authResult!.user.uid
+//                        var ref: DocumentReference? = nil
+//
+//
+//                        // Add a new document in collection "cities"
+//                        db.collection("users").document(uid).setData([
 //                            "firstName": firstName,
 //                            "last": lastName,
 //                            "uid": uid,
 //                            "apiKey": teacherAPIKey
 //                        ]) { err in
 //                            if let err = err {
-//                                print("Error adding document: \(err)")
+//                                print("Error writing document: \(err)")
 //                            } else {
-//                                print("Document added with ID: \(ref!.documentID)")
+//                                print("Document successfully written!")
 //                            }
 //                        }
+//
+//
+////                        ref = db.collection("users").addDocument(data: [
+////                            "firstName": firstName,
+////                            "last": lastName,
+////                            "uid": uid,
+////                            "apiKey": teacherAPIKey
+////                        ]) { err in
+////                            if let err = err {
+////                                print("Error adding document: \(err)")
+////                            } else {
+////                                print("Document added with ID: \(ref!.documentID)")
+////                            }
+////                        }
+//
+//                    } else {
+//                        print("Document does not exist")
+//                    }
+//                }
+//                */
+//
+//            }
+//        }
+//
+//
+//        /// Transition to home screen
+//
+//    }
+//
+    
+    func createAPIRecordForUser(with organization: String, uid: String)  {
+        
+        guard let email = emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+            let password = passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+            let firstName = firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+            let lastName = lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        else { return  }
 
-                    } else {
-                        print("Document does not exist")
-                    }
-                }
-                */
-                
-            }
-        }
-        
-        
-        /// Transition to home screen
-        
-    }
+         
+         let docRef = db.collection("apiKeys").document(organization)
+         
+         docRef.getDocument { (document, error) in
+             
+             /// handle the all the possible errors getting to the api Key
+             
+             /// - make sure the document exists if not it is a fatal error
+             guard let document = document, document.exists else {
+                 print("Document not found:"); return
+             }
+             
+             guard let record = document.data(), let teacherAPIKey = record["teacherAPIKey"] as? String else {
+                 fatalError("could not convert it to a non optional")
+             }
+             
+             ///
+//             guard let uid = Auth.auth().currentUser?.uid else {
+//                 fatalError("Could not get the uid")
+//             }
+             
+             
+             /// Add the user api key
+             let docRef = self.db.collection("users").document(uid)
+             docRef.setData(["firstName": firstName, "last": lastName, "uid": uid, "apiKey": teacherAPIKey]) { error in
+                 
+                 guard error == nil else {
+                     print("Error writing document: \(error?.localizedDescription)"); return
+                 }
+                 
+                 print("Document successfully written!")
+             }
+         }
+     }
     
     func showError(error: String)  {
         errorLabel.text = error
--- Custom Login/ViewControllers/ViewController.swift
+++ Custom Login/ViewControllers/ViewController.swift
@@ -21,13 +21,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Auth.auth().signIn(withEmail: "hertzsteven@icloud.com", password: "test1234@") { (authDataResult, error) in
-            if let error = error {
-                print("signin error", error.localizedDescription)
-            } else {
-                print("Getting the user")
-            }
-        }
+//        Auth.auth().signIn(withEmail: "hertzsteven@icloud.com", password: "test1234@") { (authDataResult, error) in
+//            if let error = error {
+//                print("signin error", error.localizedDescription)
+//            } else {
+//                print("Getting the user")
+//                switch authDataResult?.user.isEmailVerified {
+//                case true:
+//                    print("is verified")
+//                case false:
+//                    print("not verified")
+//                default:
+//                    print("in default")
+//                }
+//            }
+//        }
 
         setupElements()
     }
@@ -58,28 +66,28 @@ class ViewController: UIViewController {
         let docRef = db.collection("loginVerification").document("logins")
         
         docRef.getDocument { (document, error) in
-
-                /// handle the all the possible errors getting to the api Key
             
-                /// - make sure the dicument exists if not it is a fatal error
-                guard let document = document, document.exists else {
-                    print("Document not found:"); return
-                }
-                
-                /// - get the record dictionary  &  get the email field from the record dicionary which is also a dictionary (or map) in their lingo
-                guard let record = document.data(), let emails = record["emails"] as? [String: String] else {
-                   fatalError("Could not get the email field in the logins document")
-                }
-                
-                /// - look for the value from the dictionary with the key of ""hertzsteven@icloud.com"
-                guard let organization =  emails["hertzsteven@icloud.com"] else {
-                    print("email address is not found"); return
-                }
+            /// handle the all the possible errors getting to the api Key
+            
+            /// - make sure the dicument exists if not it is a fatal error
+            guard let document = document, document.exists else {
+                print("Document not found:"); return
+            }
+            
+            /// - get the record dictionary  &  get the email field from the record dicionary which is also a dictionary (or map) in their lingo
+            guard let record = document.data(), let emails = record["emails"] as? [String: String] else {                //                /*
+                fatalError("Could not get the email field in the logins document")
+            }
+            
+            /// - look for the value from the dictionary with the key of ""hertzsteven@icloud.com"
+            guard let organization =  emails["hertzsteven@icloud.com"] else {
+                print("email address is not found"); return
+            }
             
             /// - Now that the value is gottenyou could create Api Record for the user
             self.createAPIRecordForUser(with: organization)
-        
-    }
+            
+        }
     }
     
     func createAPIRecordForUser(with organization: String)  {
